dc2eaa866c8920b803aa8f45e5303e1a1e0b62e9
vandananar55 <vandananar55@gmail.com>
2021-12-20 01:14:26 +0530

    code for color detection added

---
+++ color-detection.py
@@ -0,0 +1,27 @@
+
+
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+
+
+img = cv2.imread("/home/anmol/Downloads/PageNo 16.jpg")
+plt.figure(figsize=(10,10))
+plt.imshow(img)
+
+
+new_image=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+plt.figure(figsize=(10,10))
+plt.imshow(new_image)
+
+hsv_image=cv2.cvtColor(new_image,cv2.COLOR_RGB2HSV)
+lower=np.array([0,100,100])
+upper=np.array([20,255,255])
+mask=cv2.inRange(hsv_image,lower,upper)
+
+plt.figure(figsize=(10,10))
+plt.imshow(mask)
+
+rest=cv2.bitwise_and(new_image,new_image,mask=mask)
+plt.figure(figsize=(10,10))
+plt.imshow(rest)
