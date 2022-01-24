4df223b972ef9a02fc28907ffdab95dcf9e803a9
vandananar55 <vandananar55@gmail.com>
2022-01-24 22:41:46 +0530

    modified image and range accordingly

--- color-detection.py
+++ color-detection.py
@@ -4,8 +4,8 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 
-
-img = cv2.imread("/home/anmol/Downloads/PageNo 16.jpg")
+# change this image path accroding to local image path
+img = cv2.imread("/home/anmol/Downloads/ne.png")
 plt.figure(figsize=(10,10))
 plt.imshow(img)
 
@@ -16,7 +16,7 @@ plt.imshow(new_image)
 
 hsv_image=cv2.cvtColor(new_image,cv2.COLOR_RGB2HSV)
 lower=np.array([0,100,100])
-upper=np.array([20,255,255])
+upper=np.array([200,255,255])
 mask=cv2.inRange(hsv_image,lower,upper)
 
 plt.figure(figsize=(10,10))
